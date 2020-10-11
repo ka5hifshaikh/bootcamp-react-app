5159103d088d295b0993499838853d6b2a466ef5
Kashif Shaikh <kscsm32@gmail.com>
2020-10-11 18:03:58 +0500

    add new component dinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Dinner from "./Dinner";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello Arisha How are You? I am Kashif</h1>
-         
-        
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello Arisha How are You? I am Kashif.</h1>
+			<Dinner dish="Chicken" />
+			<Dinner dish="Biryani" />
+			<Dinner dish="Karhai" />
+			<Dinner dish="Broast" />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Dinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Dinner(props) {
+  return (
+   <h1>I Eat {props.dish}.</h1>
+  );
+}
+
+export default Dinner;
